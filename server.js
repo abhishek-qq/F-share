4d94c486e1d44fa9b35b50f806d554d2b724f10a
Abhishek Mishra <prabhishekgeetamishra@gmail.com>
2021-11-29 08:54:41 +0530

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require ("express")
+const express = require ("express");
 const app = express();
 const ejs = require(`ejs`);
 const path = require(`path`);
+const cors = require(`cors`);
 
 //template engine
 app.set(`views` , path.join(__dirname , `/views`));
@@ -12,6 +13,14 @@ app.use(express.json());
 const connectDB =require(`./config/db`)
 connectDB();
 const PORT = process.env.PORT || 3000;
+
+//cors
+const corsOption ={
+    origin : process.env.ALLOWED_CLIENTS.split(`,`);
+
+}
+
+app.use(cors(corsOption));
 
 //routes 
 
